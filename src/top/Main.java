79a5c62512300080f8cbd945798ad952599f0fd5
KodyBird <torikura.k@gmail.com>
2017-10-02 22:28:27 +0900

    21-25

--- src/top/Main.java
+++ src/top/Main.java
@@ -7,5 +7,7 @@ public class Main {
 		//One19 one19 = new One19();
 		first.One19.Inner inner = new One19().new Inner();
 		inner.doIt();
+		first.One21_25 oneTwentyFourTwentyFive = new One21_25();
+		oneTwentyFourTwentyFive.twentyFour();
 	}
 }
